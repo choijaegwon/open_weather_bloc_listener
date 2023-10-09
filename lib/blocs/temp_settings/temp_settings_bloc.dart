8178751d184abcac48f0d1135fc21139ec2532c6
choijaegwon <chl9338@naver.com>
2023-10-09 15:09:22 +0900

    Bloc_Listener

---
+++ lib/blocs/temp_settings/temp_settings_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'temp_settings_event.dart';
+part 'temp_settings_state.dart';
+
+class TempSettingsBloc extends Bloc<TempSettingsEvent, TempSettingsState> {
+  TempSettingsBloc() : super(TempSettingsState.initial()) {
+    on<ToggleTempUnitEvent>(_toggleTempUnit);
+  }
+
+  void _toggleTempUnit(
+      ToggleTempUnitEvent event, Emitter<TempSettingsState> emit) {
+    emit(state.copyWith(
+      tempUnit: state.tempUnit == TempUnit.celsius
+          ? TempUnit.fahrenheit
+          : TempUnit.celsius,
+    ));
+  }
+}
